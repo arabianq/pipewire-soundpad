3ba7a79010baed77aec89d3795a032b0a884d1f6
arabian <a.tevg@ya.ru>
2025-07-06 21:08:50 +0300

    new app::run function

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
-use eframe::NativeOptions;
-use egui::Vec2;
 use std::fs;
-use std::fs::create_dir;
 
 mod app;
 
 fn main() -> Result<(), eframe::Error> {
     create_dirs();
-
-    let mut options = NativeOptions {
-        ..Default::default()
-    };
-    options.viewport.min_inner_size = Some(Vec2::new(400.0, 400.0));
-    options.vsync = true;
-    options.hardware_acceleration = eframe::HardwareAcceleration::Preferred;
-
-    eframe::run_native(
-        "PipeWire SoundPad",
-        options,
-        Box::new(|cc| {
-            egui_material_icons::initialize(&cc.egui_ctx);
-            Ok(Box::new(app::App::new(cc)))
-        }),
-    )
+    app::run()
 }
 
 fn create_dirs() {
