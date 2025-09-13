675f29f51f7ce2876c70bdf057d2d9a56c2edc4f
arabian <a.tevg@ya.ru>
2025-09-14 01:05:07 +0300

    remove everything

--- build_scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Change dir to the cargo project
-cd "$(dirname "$(realpath "$0")")/.." || exit
-
-RUST_TARGET=${1:-"x86_64-unknown-linux-gnu"}
-
-rustup target add "$RUST_TARGET"
-cargo build --release --target "$RUST_TARGET"
