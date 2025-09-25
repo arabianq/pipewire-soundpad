b42498d1881f8963c27758084617eca1f7fed5af
arabian <a.tevg@ya.ru>
2025-09-25 18:48:19 +0300

    remove build_rpm.sh and cargo-generate-rpm mentions. Now copr handles rpm builds

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -4,7 +4,6 @@ cd "$(dirname "$(realpath "$0")")/.." || exit
 
 bash ./scripts/build.sh
 
-bash ./scripts/build_rpm.sh
 bash ./scripts/build_deb.sh
 
 if command -v upx >/dev/null 2>&1; then
@@ -20,5 +19,4 @@ rm -rf ./target/for_github_release
 mkdir ./target/for_github_release
 
 cp "$(find ./target/debian/pwsp_*_amd64.deb | sort -V | tail -n 1)" ./target/for_github_release/
-cp "$(find ./target/generate-rpm/pwsp-*.x86_64.rpm | sort -V | tail -n 1)" ./target/for_github_release/
 zip -9j ./target/for_github_release/pwsp-x86_64-linux.zip ./target/release/pwsp-gui ./target/release/pwsp-cli ./target/release/pwsp-daemon
